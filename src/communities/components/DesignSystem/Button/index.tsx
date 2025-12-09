2012f0d7015e372af1638d6d031f4627daa73833
Mwangi800 <joseph.mwangi@digitalqatalyst.com>
2025-12-09 05:05:00 +0300

    Apply stashed deletions - remove renamed files from develop

--- src/communities/components/DesignSystem/Button/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-export { Button } from './Button';
-export type { ButtonProps } from './Button';
-export { StickyActionButton } from './StickyActionButton';
-export type { StickyActionButtonProps } from './StickyActionButton';
--- src/communities/components/DesignSystem/PageLayout/index.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export { PageLayout, PageSection, SectionHeader, SectionContent, PrimaryButton, SecondaryButton, Breadcrumbs, PageHeader } from './PageLayout';
-export type { BreadcrumbItem } from './PageLayout';
-export { HomePage, FeaturePage, DetailPage, PublicHeader, PublicFooter, Hero, ContentSection, PublicLayoutBase } from './PublicPageLayout';
